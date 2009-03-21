b79f337c51265b94b0c91b544144f30d5f24a4d7
brikkoroller <brikkoroller@3dd3ffd0-1605-11de-a379-f59b52e1acc3>
2009-03-21 11:16:06 +0000

    Fixed errors. Added sentinel. (maybe a 'bool' (typedefined) can be added to the struct mut_array saying if sentinel or not, in case of necessity).
    M    test.c

--- test.c
+++ test.c
@@ -3,32 +3,41 @@
 
 typedef struct mut_array{
   int val;
-  int * next;
+  struct mut_array * next;
 } mut_array;
 
+mut_array * sentinel;
 
-int * new() {
-  int * first = malloc(sizeof(mut_array));
-  mut_array->val = 0;
-  mut_array->next = void;
+
+mut_array * new() {
+  mut_array * first = malloc(sizeof(mut_array));
+  first->val = 0;
+  first->next = sentinel; //sould link to a sentinel
   return first;
 }
 
-void * add(int * start) {
-  int * next;
-  while((next = start->nex) != voidt) {
+void  add(mut_array * first, int val) {
+  mut_array * next;
+  while((next = first->next) != sentinel) {
     printf("searching...\r\n");
+    first = next;
   }
   next = malloc(sizeof(mut_array));
+  next->val = val;
+  next->next = sentinel;
 }
 
 void delete() {
-  
+  // should loop (while..) and free(..) every cell
+  // is there a better way of freeing the resources? 
 }
 
 int main() {
 
 
+  mut_array * myMutArray;
+  myMutArray = new();
+  //edit(<pointer to myMutArray>, <int position>, <int newValue>);
 
 
   return 0;
