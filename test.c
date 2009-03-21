70fa63b8747d41d16199f6938ae52f15547eef59
brikkoroller <brikkoroller@3dd3ffd0-1605-11de-a379-f59b52e1acc3>
2009-03-21 13:50:26 +0000

    First element works, not yet for second and so on... will continue when I'll have more time
    M    test.c

--- test.c
+++ test.c
@@ -6,13 +6,13 @@ typedef struct mut_array{
   struct mut_array * next;
 } mut_array;
 
-mut_array * sentinel;
+mut_array * sentinel = malloc(sizeof(sentinel));
 
 
 mut_array * new() {
   mut_array * first = malloc(sizeof(mut_array));
   first->val = 0;
-  first->next = sentinel; //sould link to a sentinel
+  first->next = sentinel; //should link to a sentinel
   return first;
 }
 
@@ -27,6 +27,15 @@ void  add(mut_array * first, int val) {
   next->next = sentinel;
 }
 
+void printMutArray(mut_array * first) {
+  mut_array * next;
+  printf("%d ", first->val);
+  while((next = first->next) != sentinel) {
+    printf("%d ", next->val);
+  }
+  printf("\r\n");
+}
+
 void delete() {
   // should loop (while..) and free(..) every cell
   // is there a better way of freeing the resources? 
@@ -39,6 +48,18 @@ int main() {
   myMutArray = new();
   //edit(<pointer to myMutArray>, <int position>, <int newValue>);
 
+  myMutArray->val = 1;
+  add(myMutArray, 2);
+  add(myMutArray, 3);
+
+  printMutArray(myMutArray);
+
+  //try to print second val (2):
+  mut_array * second = myMutArray->next;
+  printf("asd\r\n");
+  printf("%d %d\r\n", sentinel, second);
+  printf("second == sentinel? %d", second==sentinel);
+  printf("second: %d\r\n", second->val);
 
   return 0;
 }
